7864901d12f9927bceb8b004ce295d1e2bee11aa
Angel Daniel Morales <angelmorales@meta.com>
2025-06-18 15:59:19 -0700

    added database

---
+++ front-end/vite-project/src/Card.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import './Card.css';
+
+const Card = ({title,msg,gif,author,upvotes,id}) => {
+    const [upvotes, setUpvotes] = useState(0);
+    console.log(props)
+    //EACH BOARD DISPLAYS img,title, categoirty, view board button delete board button
+    return(
+        <div className='Card'>
+            <div className='Card-header'>
+                <h2>{props.title}</h2>
+            </div>
+            <img src={props.gif} alt ="card-gif"/>
+            <h2>{props.msg}</h2>
+            <h3>{props.author}</h3>
+            <div className="inputs">
+                <button>upvote: {upvotes}</button>
+                <button>Delete Card</button>
+            </div>
+        </div>
+
+    )
+}
+
+export default Card
